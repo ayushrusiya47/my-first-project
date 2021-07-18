c1665e7769e08c8a8c1ca7487365427cf3f56d69
Ayush Rusiya <76245068+ayushrusiya47@users.noreply.github.com>
2021-07-18 21:49:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-require('dotenv').config()
-const express=require("express");
-const cors=require("cors");
-const authRoutes=require("./routes/auth");
-const noteRoutes=require("./routes/note");
-const client=require("./configs/db");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+const noteRoutes = require("./routes/note");
+const client = require("./configs/db");
 const { connect } = require("./routes/auth");
-const app=express();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const port=process.env.port || 8000;
+const port = process.env.port || 8000;
 
- app.use('/auth',authRoutes);
- app.use('/note',noteRoutes);
+app.use("/auth", authRoutes);
+app.use("/note", noteRoutes);
 
- client.connect(()=>{
-     console.log("connected to db");
- })
-app.listen(port,()=>{
-    console.log(`server is running on port = ${port}`);
-})
+client.connect(() => {
+  console.log("connected to db");
+});
+app.listen(port, () => {
+  console.log(`server is running on port = ${port}`);
+});
 
-app.get("/", (req, res) => {
-  res.send("hello");
+app.get("/", () => {
+  console.log("hello");
 });
